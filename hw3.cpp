f8f7bab11c2f3cdf4beac8686e271c29c27a04c1
ngurro <60865695+ngurro@users.noreply.github.com>
2020-04-16 01:41:11 -0500

    Update hw3.cpp
    Added class information.

--- hw3.cpp
+++ hw3.cpp
@@ -1,4 +1,12 @@
-
+//*******************************************************************************
+// Team 18         CSCI/CMPE 1370            Spring 2020            Homework # 3
+// Nayeli Gurrola
+// Johnathan Lira
+// This program provides the user with two arithmetic operations to operate on 
+// two rational numbers given by the user. It then outputs a result in its 
+// reduced form. The user chooses whether they want to perform more operations or
+// not.
+//*******************************************************************************
 #include <iostream>
 #include "hw3functions.h"
 using namespace std;
